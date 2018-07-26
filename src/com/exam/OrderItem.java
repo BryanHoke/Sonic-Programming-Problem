85c9b40f402aa3c9b791b04dbe5a2957b9338b77
Bryan Hoke <zinth@icloud.com>
2018-07-26 12:48:18 -0500

    Implement equals and hashCode for OrderItem.

--- src/com/exam/OrderItem.java
+++ src/com/exam/OrderItem.java
@@ -42,4 +42,34 @@ public abstract class OrderItem {
 	 * @return True if the Item is taxable, false otherwise.
 	 */
 	public abstract boolean isTaxable();
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((item == null) ? 0 : item.hashCode());
+		result = prime * result + quantity;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		if (item == null) {
+			if (other.item != null)
+				return false;
+		} else if (!item.equals(other.item))
+			return false;
+		if (quantity != other.quantity)
+			return false;
+		return true;
+	}
 }

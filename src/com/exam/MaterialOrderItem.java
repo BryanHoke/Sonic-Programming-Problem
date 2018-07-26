48e3271ee40a17def41ab9e15553558d68a3c7fa
Bryan Hoke <zinth@icloud.com>
2018-07-26 12:36:16 -0500

    Declare MaterialOrderItem.

---
+++ src/com/exam/MaterialOrderItem.java
@@ -0,0 +1,21 @@
+package com.exam;
+
+/**
+ * Represents a material item in an order.
+ * 
+ * @author bryanhoke
+ *
+ */
+public class MaterialOrderItem extends OrderItem {
+	
+	public MaterialOrderItem(Item item, int quantity)
+	{
+		super(item, quantity);
+	}
+
+	@Override
+	public boolean isTaxable() {
+		return true;
+	}
+
+}
